6333ea8ff19aa4a635d119217e273551dbc2b7b0
Online Primacy <online.primacy@gmail.com>
2025-09-27 01:59:32 +0530

    Initial commit - Shri Stuti website

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import ChatPreview from "./components/ChatPreview";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <h1>Welcome to Shri Stuti 🙏</h1>
+      <ChatPreview />
+    </>
+  );
+}
---
+++ components/ChatPreview.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ChatPreview() {
+  return (
+    <div style={{ padding: "1rem", textAlign: "center" }}>
+      <p>Here you’ll find Aartis, Mantras, Bhajans & more soon…</p>
+    </div>
+  );
+}
